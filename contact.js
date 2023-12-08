ca2f56ca452a557938971810547c50a780173ae4
mac <a12@Shahils-MacBook-Pro.local>
2023-12-08 16:33:17 +0530

    first commit

---
+++ contact.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.hamburger')
+const navText = document.querySelector('.nav-text')
+
+
+hamburger.addEventListener('click', ()=>{
+    navText.classList.toggle('active')
+})
--- script.js
+++ script.js
@@ -1,33 +1,27 @@
 const seachbutton = document.querySelector('.button');
+const searchResultsContainer = document.querySelector('.results')
 
 const api_random = "https://www.themealdb.com/api/json/v1/1/random.php";
 const randomMealText = document.querySelector('#recommended-meal-title');
 
-
-
-
-
-
-
-// const ingredientList = document.querySelector('.ingredient-list')
-const viewIngredients = document.querySelectorAll('.recommended-meal-ingredients');
 const modalContainer = document.querySelector('.modal-container');
+const viewIngredients = document.querySelectorAll('.recommended-meal-ingredients');
 const closeButton = document.querySelector('.close');
 const modalTitle = document.querySelector('.modal-title');
 const modalImage = document.querySelector('.modal-img');
+const ingredientList = document.querySelector('.ingredient-list')
 
-viewIngredients.forEach(function (viewIngredients) {
-    viewIngredients.addEventListener('click', () => {
-        modalContainer.classList.add('show');
-    });
-
-});
-
-closeButton.addEventListener('click', () => {
-    modalContainer.classList.remove('show');
-});
+const main = document.querySelector('.main')
+const nav = document.querySelector('nav')
+const logobox = document.querySelector('.logo-box')
+const footer = document.querySelector('footer')
+const hamburger = document.querySelector('.hamburger')
+const navText = document.querySelector('.nav-text')
 
 
+hamburger.addEventListener('click', ()=>{
+    navText.classList.toggle('active')
+})
 // FETCHING AND GENERATING USER SEARCH RESULTS
 
 seachbutton.addEventListener('click', () => {
@@ -43,25 +37,82 @@ seachbutton.addEventListener('click', () => {
 
 
     function generateSearchResults(data) {
+        const allIngredientss = [];
 
         const resultImageContainers = document.querySelectorAll('.resultimg')
         const resultMealNames = document.querySelectorAll('.result-title')
 
 
         for (let i = 0; i < 6; i++) {
-            // console.log('test')
 
             data.meals.slice(0, 12).forEach((element, i) => {
                 resultImageContainers[i].style.backgroundImage = `url(${element.strMealThumb})`;
                 resultMealNames[i].innerHTML = `${element.strMeal}`
 
-                // modalImage[i].style.backgroundImage = `url(${element.strMealThumb})`;
-                // modalTitle[i].innerHTML = `${element.strMeal}`
+                modalImage.style.backgroundImage = `url(${element.strMealThumb})`
+                modalTitle.innerHTML = `${element.strMeal}`
+
+                // for (let i = 1; i <= 20; i++) {
+                //     const ingredient = element[`strIngredient${i}`];
+                //     const measure = element[`strMeasure${i}`];
+                //     if (ingredient && measure) {
+                //         allIngredientss.push(`<li>${measure} ${ingredient}</li>`);
+                //     }
+                // }
+                // ingredientList.innerHTML = allIngredientss
 
             });
         }
 
+
+
     }
+
+    function generateIngredients(data) {
+
+
+        viewIngredients.forEach(function (viewIngredients) {
+            viewIngredients.addEventListener('click', () => {
+                modalContainer.classList.add('show');
+                main.style.filter = 'blur(3px)'
+                nav.style.filter = 'blur(3px)'
+                logobox.style.filter = 'blur(3px)'
+                footer.style.filter = 'blur(3px)'
+
+            });
+
+        });
+
+        closeButton.addEventListener('click', () => {
+            modalContainer.classList.remove('show');
+            main.style.filter = 'blur(0px)'
+            nav.style.filter = 'blur(0px)'
+            logobox.style.filter = 'blur(0px)'
+            footer.style.filter = 'blur(0px)'
+        });
+
+
+        searchResultsContainer.addEventListener('click', function (event) {
+            const clickedElement = event.target;
+
+            if (clickedElement.tagName === 'H3') {
+                // const imageUrl = clickedElement.src;
+                const foodName = clickedElement.parentElement;
+                fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
+                    .then(response => response.json())
+                    .then(data => {
+                        const meal = data.meals[0];
+                        const ingredients = meal.strIngredient1 + ', ' + meal.strIngredient2 + ', ' + meal.strIngredient3 + ', ' + meal.strIngredient4 + ', ' + meal.strIngredient5;
+                        console.log(foodName)
+                    })
+                    .catch(error => {
+                        console.error('Error fetching ingredients:', error);
+                    });
+            }
+        });
+
+    }
+
 
 
     fetch(api_category)
@@ -71,22 +122,56 @@ seachbutton.addEventListener('click', () => {
         .then(function (data) {
             console.log("Result is: ", data);
             generateSearchResults(data);
+            generateIngredients(data)
         })
 
 })
 
 
+
+
+
+
+
+
 // FETCHING AND GENERATING RANDOM MEALS
 
 function generateRandomMeal(data) {
+    const allIngredientss = [];
+
     const randomImageBox = document.querySelector('.recommended-img-div')
 
     data.meals.forEach(element => {
         randomMealText.innerHTML = `${element.strMeal}`
         randomImageBox.style.backgroundImage = `url(${element.strMealThumb})`
-    });
-    console.log(data.meals)
 
+        modalImage.style.backgroundImage = `url(${element.strMealThumb})`
+        modalTitle.innerHTML = `${element.strMeal}`
+
+
+        // for (let i = 1; i <= 20; i++) {
+        //     const ingredient = element[`strIngredient${i}`];
+        //     const measure = element[`strMeasure${i}`];
+        //     if (ingredient && measure) {
+        //         allIngredientss.push(`<li>${measure} ${ingredient}</li>`);
+        //     }
+        // }
+        // ingredientList.innerHTML = allIngredientss
+
+
+        var output = ''
+        for (let i = 1; i <= 20; i++) {
+            const ingredient = element[`strIngredient${i}`];
+            const measure = element[`strMeasure${i}`];
+            if (ingredient && measure) {
+                output += `<li>${measure} ${ingredient}</li>`
+            }
+        }
+        ingredientList.innerHTML = output
+
+
+    });
+    console.log(allIngredientss)
 
 }
 
@@ -98,7 +183,8 @@ fetch(api_random)
     .then(function (data) {
         console.log('data:', data)
         generateRandomMeal(data)
-        // randomMealText.innerHTML= this.data.meals.strMeal
     })
+
+
 
 
